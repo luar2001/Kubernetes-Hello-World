6b387716682e525e348854ae18662b99ee8bdd14
lukas <lukas.aronsson@protonmail.com>
2021-09-16 15:45:34 +0200

    added spring-boot and maven

---
+++ src/test/java/com/example/Kubernetesdemo/KubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kubernetesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
